b8ee4c27769dbbffbe2fe3d38f61468d15d975ed
bakuzan <imannoyed4days@hotmail.co.uk>
2018-03-09 10:55:38 +0000

    reverse graveyards when board reverses

--- src/containers/Graveyard.js
+++ src/containers/Graveyard.js
@@ -4,17 +4,19 @@ import { connect } from 'react-redux';
 import Graveyard from 'components/graveyard/Graveyard';
 
 import Constants from 'constants/index';
+import { isWhitesTurn } from 'utils/game';
 
-const DualGraveyards = ({ whitePieces, blackPieces }) => (
+const DualGraveyards = ({ isReversed, whitePieces, blackPieces }) => (
   <div id="paired-graveyards">
-    <Graveyard pieces={whitePieces} />
-    <Graveyard pieces={blackPieces} />
+    <Graveyard pieces={isReversed ? blackPieces : whitePieces} />
+    <Graveyard pieces={isReversed ? whitePieces : blackPieces} />
   </div>
 );
 
 const getPiecesForColour = (state, colour) =>
   state.board.graveyard.filter(x => x.colour === colour);
 const mapStateToProps = state => ({
+  isReversed: !isWhitesTurn(state.board.moves.length),
   whitePieces: getPiecesForColour(state, Constants.Strings.colours.white),
   blackPieces: getPiecesForColour(state, Constants.Strings.colours.black)
 });
