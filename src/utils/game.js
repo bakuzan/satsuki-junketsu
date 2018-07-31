702bab8e5a21f1161a10f4998477c2236e67591d
bakuzan <imannoyed4days@hotmail.co.uk>
2018-07-31 14:25:43 +0100

    fix pawn jump bug..requires test.

--- src/utils/game.js
+++ src/utils/game.js
@@ -2,14 +2,15 @@ import Constants from 'constants/index';
 
 import { isSameSquare, isOnDiagonal, isBetween } from './board';
 
-export const isWhitesTurn = moveCount => moveCount === 0 || moveCount % 2 === 0;
+export const isWhitesTurn = (moveCount) =>
+  moveCount === 0 || moveCount % 2 === 0;
 
-export const getCurrentPlayerColour = moves =>
+export const getCurrentPlayerColour = (moves) =>
   isWhitesTurn(moves.length)
     ? Constants.Strings.colours.white
     : Constants.Strings.colours.black;
 
-export const getWinningPlayerColour = kingSquare =>
+export const getWinningPlayerColour = (kingSquare) =>
   kingSquare.contains.colour === Constants.Strings.colours.white
     ? Constants.Strings.colours.black
     : Constants.Strings.colours.white;
@@ -21,14 +22,14 @@ export const hasFreePath = (from, to, match, squares) => {
     if (square.contains === null) continue;
     if (isSameSquare(from, square)) continue;
 
-    const fileIndex = Constants.files.findIndex(x => x === square.file);
+    const fileIndex = Constants.files.findIndex((x) => x === square.file);
     if (hasDiagonalMovement) {
       if (isOnDiagonal(fileIndex, from, to, square)) return false;
     } else if (match.files && square.file === to.file) {
       if (isBetween(from.rank, to.rank, square.rank)) return false;
     } else if (match.ranks && square.rank === to.rank) {
-      const fromFileIndex = Constants.files.findIndex(x => x === from.file);
-      const toFileIndex = Constants.files.findIndex(x => x === to.file);
+      const fromFileIndex = Constants.files.findIndex((x) => x === from.file);
+      const toFileIndex = Constants.files.findIndex((x) => x === to.file);
       if (isBetween(fromFileIndex, toFileIndex, fileIndex)) return false;
     }
   }
@@ -40,8 +41,8 @@ export const isValidMove = (from, to, squares) => {
     files: from.file === to.file,
     ranks: from.rank === to.rank
   };
-  const fromFileIndex = Constants.files.findIndex(x => x === from.file);
-  const toFileIndex = Constants.files.findIndex(x => x === to.file);
+  const fromFileIndex = Constants.files.findIndex((x) => x === from.file);
+  const toFileIndex = Constants.files.findIndex((x) => x === to.file);
   const fileDiff = Math.abs(toFileIndex - fromFileIndex);
   const rankDiff = Math.abs(to.rank - from.rank);
 
@@ -49,10 +50,14 @@ export const isValidMove = (from, to, squares) => {
     case Constants.Strings.pieces.pawn:
       if (!match.files) return false;
       if (piece.colour === Constants.Strings.colours.white) {
-        if (from.rank === 2 && [3, 4].indexOf(to.rank) !== -1) return true;
+        if (from.rank === 2 && 3 === to.rank) return true;
+        if (from.rank === 2 && 4 === to.rank)
+          return hasFreePath(from, to, match, squares);
         if (rankDiff === 1 && from.rank < to.rank) return true;
       } else if (piece.colour === Constants.Strings.colours.black) {
-        if (from.rank === 7 && [6, 5].indexOf(to.rank) !== -1) return true;
+        if (from.rank === 7 && 6 === to.rank) return true;
+        if (from.rank === 7 && 5 === to.rank)
+          return hasFreePath(from, to, match, squares);
         if (rankDiff === 1 && from.rank > to.rank) return true;
       }
       return false;
@@ -85,14 +90,16 @@ export const isValidMove = (from, to, squares) => {
 
 export const isValidTake = (from, to, squares) => {
   const { contains: attacker } = from;
-  const toSquare = squares.find(x => x.file === to.file && x.rank === to.rank);
+  const toSquare = squares.find(
+    (x) => x.file === to.file && x.rank === to.rank
+  );
   if (!toSquare || !toSquare.contains) return false;
   if (toSquare.contains.colour === attacker.colour) return false;
 
   switch (attacker.name) {
     case Constants.Strings.pieces.pawn:
-      const fromIndex = Constants.files.findIndex(x => x === from.file);
-      const toIndex = Constants.files.findIndex(x => x === to.file);
+      const fromIndex = Constants.files.findIndex((x) => x === from.file);
+      const toIndex = Constants.files.findIndex((x) => x === to.file);
       return (
         Math.abs(toIndex - fromIndex) === 1 &&
         Math.abs(to.rank - from.rank) === 1 &&
