e007264046fb6acc26661e38477bfac5376025a7
bakuzan <imannoyed4days@hotmail.co.uk>
2018-12-04 09:21:15 +0000

    better consistant ids.

--- src/utils/board.js
+++ src/utils/board.js
@@ -1,21 +1,32 @@
 import Constants from 'constants/index';
-import { generateUniqueId } from './common';
 
 const { pieces, colours } = Constants.Strings;
 
 const getStartingPiece = (rank, file) => {
   if ([1, 2, 7, 8].indexOf(rank) === -1) return null;
 
-  const piece = {
-    id: generateUniqueId(),
-    colour: rank < 5 ? colours.white : colours.black,
+  let piece = '',
+    pieceId = '';
+  const colour = rank < 5 ? colours.white : colours.black;
+
+  if ([2, 7].indexOf(rank) > -1) {
+    [piece, pieceId] = [pieces.pawn, 'p'];
+  } else if (['a', 'h'].indexOf(file) > -1) {
+    [piece, pieceId] = [pieces.rook, 'r'];
+  } else if (['b', 'g'].indexOf(file) > -1) {
+    [piece, pieceId] = [pieces.knight, 'n'];
+  } else if (['c', 'f'].indexOf(file) > -1) {
+    [piece, pieceId] = [pieces.bishop, 'b'];
+  } else {
+    [piece, pieceId] = file === 'd' ? [pieces.queen, 'q'] : [pieces.king, 'k'];
+  }
+
+  return {
+    id: `${colour.slice(0, 1)}-${pieceId}-${file}`,
+    name: piece,
+    colour,
     hasMoved: false
   };
-  if ([2, 7].indexOf(rank) > -1) return { ...piece, name: pieces.pawn };
-  if (['a', 'h'].indexOf(file) > -1) return { ...piece, name: pieces.rook };
-  if (['b', 'g'].indexOf(file) > -1) return { ...piece, name: pieces.knight };
-  if (['c', 'f'].indexOf(file) > -1) return { ...piece, name: pieces.bishop };
-  return { ...piece, name: file === 'd' ? pieces.queen : pieces.king };
 };
 
 export const buildStartingBoard = () => {
@@ -53,13 +64,13 @@ export const isBetween = (num1, num2, middleNumber) => {
 };
 
 export const isOnDiagonal = (fileIndex, from, to, square) => {
-  const fromFileIndex = Constants.files.findIndex(x => x === from.file);
+  const fromFileIndex = Constants.files.findIndex((x) => x === from.file);
   const fileDiff = Math.abs(fileIndex - fromFileIndex);
   const rankDiff = Math.abs(square.rank - from.rank);
   if (!(fileDiff === rankDiff)) return false;
   if (!isBetween(from.rank, to.rank, square.rank)) return false;
 
-  const toFileIndex = Constants.files.findIndex(x => x === to.file);
+  const toFileIndex = Constants.files.findIndex((x) => x === to.file);
   if (!isBetween(fromFileIndex, toFileIndex, fileIndex)) return false;
   return true;
 };
