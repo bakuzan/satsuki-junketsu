ff10299853cefb9cc54d940885833277e5713b12
bakuzan <imannoyed4days@hotmail.co.uk>
2018-12-04 10:04:37 +0000

    added app info.

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 
 import Header from 'components/header/Header';
 import Toaster from 'components/toaster/Toaster';
+import AppInformation from 'components/appInformation';
 
-const App = ({ children }) => (
-  <div className="satsuki">
-    <Header />
-    <main>{children}</main>
-    <Toaster />
-  </div>
-);
+const App = React.memo(function App({ children }) {
+  return (
+    <div className="satsuki">
+      <Header />
+      <main>{children}</main>
+      <Toaster />
+      <AppInformation />
+    </div>
+  );
+});
 
 export default App;
