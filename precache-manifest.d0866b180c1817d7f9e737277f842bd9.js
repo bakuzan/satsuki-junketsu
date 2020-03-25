da8909d2999ffd1cc7d86c9b669f3d07fc4ba6f5
bakuzan <imannoyed4days@hotmail.co.uk>
2020-03-25 08:10:38 +0000

    Updates

---
+++ precache-manifest.d0866b180c1817d7f9e737277f842bd9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffc1075ef430af415e514264cd773cb7",
+    "url": "/satsuki-junketsu/index.html"
+  },
+  {
+    "revision": "ef9d08b4b2f30bd21739",
+    "url": "/satsuki-junketsu/static/css/main.81d2e307.chunk.css"
+  },
+  {
+    "revision": "071e9284ab6ba19ec02b",
+    "url": "/satsuki-junketsu/static/js/2.d7cf5745.chunk.js"
+  },
+  {
+    "revision": "b288553b0b8be16072c7792644e51c51",
+    "url": "/satsuki-junketsu/static/js/2.d7cf5745.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef9d08b4b2f30bd21739",
+    "url": "/satsuki-junketsu/static/js/main.5c7b4946.chunk.js"
+  },
+  {
+    "revision": "6f7534f0d6337966d5c6",
+    "url": "/satsuki-junketsu/static/js/runtime-main.d1bce533.js"
+  },
+  {
+    "revision": "e92cc0fb9e1a7debc138224fd02a462a",
+    "url": "/satsuki-junketsu/static/media/roboto-mono.e92cc0fb.woff2"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/satsuki-junketsu/precache-manifest.d68f0ed1cd4ae9626fc0767bd126e935.js"
+  "/satsuki-junketsu/precache-manifest.d0866b180c1817d7f9e737277f842bd9.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/satsuki-junketsu/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/satsuki-junketsu/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
